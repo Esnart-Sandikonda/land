08e2edbb7c9db89ce945dd3c429cc554834680ee
GeorgeFatsani-21 <bsc-inf-12-18@unima.ac.mw>
2023-05-21 13:02:18 +0200

    progress

---
+++ land/src/components/Estates.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Estates.css";
+import Sidebar from "./Sidebar";
+import SideNavbar from "./UserSideNavbar"
+
+
+
+function Estates(){
+
+    return (
+        <div>
+            <Sidebar/>
+            <SideNavbar/>
+        
+        </div>
+    );
+    
+}
+
+export default Estates;
---
+++ land/src/components/Userlandpage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Userlandpage.css";
+import Userbar from "./Userbar";
+import UserSideNavbar from "./UserSideNavbar"
+
+
+
+function Userlandpage(){
+
+    return (
+        <div>
+            <Userbar/>
+            <UserSideNavbar/>
+        
+        </div>
+    );
+}
+
+export default Userlandpage;
